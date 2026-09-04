4decc14a88561d722ae42025e46190055382c471
Siva <pmrlsivas@gmail.com>
2026-09-04 21:40:38 +0530

    db

--- sling-assets/admin-panel/Dbconfig.php
+++ sling-assets/admin-panel/Dbconfig.php
@@ -1,4 +1,4 @@
 <?php
-$connect = new PDO('mysql:host=127.0.0.1:3307;dbname=sling-website', 'root', '');
+$connect = new PDO('mysql:host=127.0.0.1:3306;dbname=sling-website', 'root', '');
 // $connect = new PDO('mysql:host=localhost;dbname=slinggro_slingwebsite', 'slinggro_slingwebsite', '_MzK)ZfDL%t6');
 ?>
